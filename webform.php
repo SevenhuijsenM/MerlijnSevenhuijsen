924d700f6cd74464c9891fbe007ab110820173b1
SevenhuijsenM <m.a.l.sevenhuijsen@student.tue.nl>
2021-04-21 12:32:31 +0200

    Contact form update
    First try on fixing the webform php
    Added a subject to the contact form.

--- webform.php
+++ webform.php
@@ -1,119 +1,58 @@
 <?php
-echo"<pre>";
-    print_r($_POST);
-echo"</pre>";
-    $message_sent = false;
-    if ((isset($_POST['email']) && $_POST['email'] != '') && (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)))  {
-        // Submit the form
-        $userName = $_POST['name'];
-        $userEmail = $_POST['email'];
-        $message = $_POST['message'];
-    
-        $to = "contact@merlijnsevenhuijsen.com"
-        $body ="";
-        
-        $body.= "From: ".userName. "\r\n";
-        $body.= "Email: ".userEmail. "\r\n";
-        $body.= "Message: ".message. "\r\n";
-    
-        mail($to, "New contact form", $body)
-
-        $message_sent = true;
-    } else {
-        $invalid_class_name = "form-invalid";
+  
+if($_POST) {
+    $name = "";
+    $email = "";
+    $title = "";
+    $message = "";
+    $email_body = "<div>";
+      
+    if (isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;
+                           <span>".$name."</span> 
+                        </div>";
     }
+ 
+    if (isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $body .= "  <div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
+                    </div>";
+    }
+      
+    if(isset($_POST['title'])) {
+        $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$title."</span>
+                        </div>";
+    }
+      
+    if (isset($_POST['message'])) {
+        $visitor_message = htmlspecialchars($_POST['message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$message."</div>
+                        </div>";
+    }
+      
+    $email_recipient = "contact@merlijnsevenhuijsen.com";
+      
+    $email_body .= "</div>";
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+      
+    if (mail($recipient, $title, $email_body, $headers)) {
+        echo "<p>Thank you for contacting us, $name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Contact</title>
-        <meta name="description" conctent="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
-    </head>
-    <body>
-        <?php 
-        if ($message_sent):
-        ?> 
-            <h3> Thanks, I will be in touch.</h3>
-        <?php
-        else:
-        ?>
-        <nav class="navigation-bar full-screen-fixed flex-around">
-            <div class="logo">
-                <a href="index.html">Merlijn Sevenhuijsen</a>
-            </div>
-            <ul class="nav-links flex-around">
-                <li class="flex-around">
-                    <a href="index.html">Home</a>
-                </li>
-                <li class="flex-around">
-                    <a href="about.html">About me</a>
-                </li>
-                <li class="flex-around">
-                    <a href="projects.html">Projects</a>
-                </li>
-                <li class="color-contact flex-around">
-                    <a href="contact.html">Contact</a>
-                </li class="flex-around">
-                <li class="flex-around" id="li-login">
-                    <a href="#"><span>Log In</span></a> 
-                </li>
-            </ul>
-            <div class="burger">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-        </nav>
-        <div class="page-bar color-contact"></div>
-        <div class="container" id="container-contact">
-            <div class="background-image" id="background-image-contact"></div>
-            <section class="contact-section flex-around">
-                <div class="section-box">
-                    <div class="square glass-box" style="--i:0"></div>
-                    <div class="square glass-box" style="--i:1"></div>
-                    <div class="square glass-box" style="--i:2"></div>
-                    <div class="square glass-box" style="--i:3"></div>
-                    <div class="square glass-box" style="--i:4"></div>
-                    <div class="form-container glass-box">
-                        <div class="form">
-                            <h2>Contact Me</h2>
-                            <form action="webform.php" id="contact-form" method="POST">
-                                <div class="inputBx">
-                                    <input name="name" type="text" class="glass-box" placeholder="Full Name">
-                                </div>
-                                <div class="inputBx">
-                                    <input name="email" type="email" class="glass-box <?= $invalid_class_name ?? "" ?>" placeholder="Email">
-                                </div>
-                                <div class="inputBx">
-                                    <textarea name="message" class="glass-box" placeholder="Type Message Here..."></textarea>
-                                </div>
-                                <div class="inputBx">
-                                    <input type="submit" value="Send">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-        <footer class="bottom-bar full-screen-fixed flex-around">
-            <p id="copyright">&#169; 2021 Merlijn Sevenhuijsen</p>
-            <ul class="social-media flex-around">
-                <li><a class="flex-around" href="https://www.facebook.com/merlijn.sevenhuijsen/"><img src="background_images/facebook.png" alt="Facebook link"></a></li>
-                <li><a class="flex-around" href="https://twitter.com/merlijndussen/"><img src="background_images/twitter.svg" alt="Twitter link"></a></li>
-                <li><a class="flex-around" href="https://www.instagram.com/merlijndussen/"><img src="background_images/instagram.png" alt="Instagram link"></a></li>
-                <li><a class="flex-around" id="gitH" href="https://github.com/SevenhuijsenM"><img src="background_images/github.png" alt="Github link"></a></li>
-            </ul>
-        </footer>
-        <script src="checkEmail.js"></script>
-        <script src="app.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
-    </body>
-    <?php
-    endif;
-    ?>
-</html>
